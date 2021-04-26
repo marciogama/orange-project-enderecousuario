84886c24ffe61315bbc676327b34251c887c40a0
Marcio Gama <marciogama2000@gmail.com>
2021-04-26 18:43:13 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/projetoOrange/enderecousuario/EnderecousuarioApplication.java
@@ -0,0 +1,13 @@
+package com.projetoOrange.enderecousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecousuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecousuarioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetoOrange/enderecousuario/EnderecousuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoOrange.enderecousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecousuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
