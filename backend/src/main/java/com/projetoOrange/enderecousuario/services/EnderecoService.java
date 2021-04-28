c160c79d7bba4c973f451f9678bd9b96690a071e
Marcio Gama <marciogama2000@gmail.com>
2021-04-28 08:26:35 -0300

    Database seeding

--- backend/src/main/java/com/projetoOrange/enderecousuario/services/EnderecoService.java
+++ backend/src/main/java/com/projetoOrange/enderecousuario/services/EnderecoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetoOrange.enderecousuario.entities.Endereco;
 import com.projetoOrange.enderecousuario.repositories.EnderecoRepository;
@@ -14,6 +15,7 @@ public class EnderecoService {
 	@Autowired
 	private EnderecoRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Endereco> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_Endereco (logradouro, numero,complemento,bairro,cidade,estado,cep) VALUES ('Rua do Catete', '340', 'Sala 301', 'Catete', 'Rio de Janeiro','RJ','20210045');
+INSERT INTO tb_Endereco (logradouro, numero,complemento,bairro,cidade,estado,cep) VALUES ('Rua Barata Ribeiro', '850', 'Apt. 304', 'Copacabana', 'Rio de Janeiro','RJ','22210045');
+		
