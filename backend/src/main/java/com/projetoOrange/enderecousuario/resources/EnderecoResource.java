6a7ac6d9f45c5f9054fe3ea2d351e653a08d573e
Marcio Gama <marciogama2000@gmail.com>
2021-04-29 12:39:02 -0300

    inclusao de novo endereco

--- backend/src/main/java/com/projetoOrange/enderecousuario/resources/EnderecoResource.java
+++ backend/src/main/java/com/projetoOrange/enderecousuario/resources/EnderecoResource.java
@@ -1,12 +1,17 @@
 package com.projetoOrange.enderecousuario.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetoOrange.enderecousuario.dto.EnderecoDTO;
 import com.projetoOrange.enderecousuario.services.EnderecoService;
@@ -23,4 +28,18 @@ public class EnderecoResource {
 		List<EnderecoDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<EnderecoDTO> findById(@PathVariable Long id) {
+		EnderecoDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EnderecoDTO> insert(@RequestBody EnderecoDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/projetoOrange/enderecousuario/services/EnderecoService.java
+++ backend/src/main/java/com/projetoOrange/enderecousuario/services/EnderecoService.java
@@ -1,6 +1,7 @@
 package com.projetoOrange.enderecousuario.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projetoOrange.enderecousuario.dto.EnderecoDTO;
 import com.projetoOrange.enderecousuario.entities.Endereco;
 import com.projetoOrange.enderecousuario.repositories.EnderecoRepository;
+import com.projetoOrange.enderecousuario.services.exceptions.EntityNotFoundException;
 
 @Service
 public class EnderecoService {
@@ -25,5 +27,29 @@ public class EnderecoService {
 		
 		return listDto;
 	}
+		
+	@Transactional(readOnly = true)
+	public EnderecoDTO findById(Long id) {
+		Optional <Endereco> obj = repository.findById(id);
+		Endereco entity = obj.orElseThrow(() -> new EntityNotFoundException("Código não encontrado !"));
+		
+		return new EnderecoDTO(entity);
+	}
+
+	@Transactional
+	public EnderecoDTO insert(EnderecoDTO dto) {
+		Endereco entity = new Endereco();
+		entity.setLogradouro(dto.getLogradouro());
+		entity.setNumero(dto.getNumero());
+		entity.setComplemento(dto.getComplemento());
+		entity.setBairro(dto.getBairro());
+		entity.setCidade(dto.getCidade());
+		entity.setEstado(dto.getEstado());
+		entity.setCep(dto.getCep());
+		entity = repository.save(entity);
+		return new EnderecoDTO(entity);
+	}
+	
+	
 
 }
